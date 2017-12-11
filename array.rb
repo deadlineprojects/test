ff62e07a2d65043a9bdbad850a00f7b48d53bae2
DEADLINEPROJECTS <dl.project@yandex.ru>
2017-12-11 13:54:07 +0300

    Добавлены новые строки и массив имен

--- array.rb
+++ array.rb
@@ -1,4 +1,6 @@
 #Работаем с массивами
 a = [1, 2, 4, 5, 2, 1, 6, 11]
 puts a.select.with_index {|_, index| index.even? }
-putsa.select.with_index {|_,index| index.odd?}
+puts a.select.with_index {|_,index| index.odd?}
+b= ["Саша", "Алеша", "Петя", "Витя", "Маша"]
+puts и.select.with_index {|_, index| index.even? }
