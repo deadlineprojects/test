d68c5467362d02511f1b24224ff0f263efec9e97
DEADLINEPROJECTS <dl.project@yandex.ru>
2017-12-11 13:42:10 +0300

    added new line

--- array.rb
+++ array.rb
@@ -1,3 +1,4 @@
 #Работаем с массивами
 a = [1, 2, 4, 5, 2, 1, 6, 11]
-puts a.select.with_index {|_, index| index.even? }+a.select.with_index {|_,index| index.odd?}
+puts a.select.with_index {|_, index| index.even? }
+putsa.select.with_index {|_,index| index.odd?}
